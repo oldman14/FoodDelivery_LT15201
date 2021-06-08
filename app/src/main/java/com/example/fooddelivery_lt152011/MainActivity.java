8ba7f320a1c10d2834d49dce0858bfef10ce990e
HoangThai <thaibuaplol@gmail.com>
2021-06-08 11:58:01 +0700

    BottomNavigationView

--- app/src/main/java/com/example/fooddelivery_lt152011/MainActivity.java
+++ app/src/main/java/com/example/fooddelivery_lt152011/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.fooddelivery_lt152011;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.fooddelivery_lt152011.AccountScreen.AccountFragment;
+import com.example.fooddelivery_lt152011.HomeScreen.HomeFragment;
+import com.example.fooddelivery_lt152011.ProductScreen.ProductFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigation);
+        loadFragment(new HomeFragment());
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigation
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_product:
+                    fragment = new ProductFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_account:
+                    fragment = new AccountFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+    private void loadFragment (Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
+
