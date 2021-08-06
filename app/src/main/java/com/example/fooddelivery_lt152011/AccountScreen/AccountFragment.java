fbd01523ce1ab7d43764e90b18d8c7ea1d7af0fc
nvtuyen2k <vantuyen.nvt1999@gmail.com>
2021-08-07 01:57:33 +0700

    edit lại profile user

--- app/src/main/java/com/example/fooddelivery_lt152011/AccountScreen/AccountFragment.java
+++ app/src/main/java/com/example/fooddelivery_lt152011/AccountScreen/AccountFragment.java
@@ -17,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
@@ -35,18 +36,30 @@ import com.example.fooddelivery_lt152011.R;
 
 import com.example.fooddelivery_lt152011.networking.Http.HttpAdapter;
 import com.example.fooddelivery_lt152011.networking.Service.SystemService;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.UUID;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 public class AccountFragment extends Fragment {
-    CardView cvMyProfile, cvFavoriteFood;
+    CardView cvMyProfile, cvFavoriteFood,question;
     private LinearLayout logout;
     TextInputEditText edtName;
-    private ImageView edtimage, imgAvatar;
+     private CircleImageView edtimage,imgAvatar;
+    private ImageView imgcapture;
     TextView tvSĐT, tvNameUser;
     private static final int Image_Capture_Code = 1;
     public static final String url = HTTP_URL.Final_URL;
@@ -54,160 +67,164 @@ public class AccountFragment extends Fragment {
     public static SystemService action;
     UserDAO dao;
     Uri uri;
+    String linkImage;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_account, container, false);
-        cvMyProfile = view.findViewById(R.id.cvMyProfile);
-        logout = view.findViewById(R.id.buttonLogOut);
-        tvSĐT = view.findViewById(R.id.tvsdt);
-        cvFavoriteFood = view.findViewById(R.id.cvFavoriteFood);
-        imgAvatar = view.findViewById(R.id.imgAvatar);
-        tvNameUser = view.findViewById(R.id.tvNameUser);
+        MainActivity.toolbar_logo.setVisibility( View.GONE );
+        View view = inflater.inflate( R.layout.fragment_account, container, false );
+        cvMyProfile = view.findViewById( R.id.cvMyProfile );
+        logout = view.findViewById( R.id.buttonLogOut );
+        tvSĐT = view.findViewById( R.id.tvsdt );
+        question=view.findViewById( R.id.question );
+        cvFavoriteFood = view.findViewById( R.id.cvFavoriteFood );
+        imgAvatar = view.findViewById( R.id.imgAvatar );
+        tvNameUser = view.findViewById( R.id.tvNameUser );
         dao = new UserDAO();
-        dbHelper = new DbHelper(getActivity());
+        dbHelper = new DbHelper( getActivity() );
         ModelUser nameimg = dbHelper.getUser();
-        tvNameUser.setText(nameimg.getUserName());
-
-        tvSĐT.setText("0" + nameimg.getUserPhone());
-
-
-        logout.setOnClickListener(new View.OnClickListener() {
+        Picasso.get().load( nameimg.getUserImage() ).into( imgAvatar );
+        tvNameUser.setText( nameimg.getUserName() );
+        tvSĐT.setText( "0" + nameimg.getUserPhone() );
+        logout.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbHelper.delete();
-                Intent i = new Intent(getActivity(), SendOTPActivity.class);
-                startActivity(i);
+                Intent i = new Intent( getActivity(), SendOTPActivity.class );
+                startActivity( i );
             }
-        });
+        } );
 
-        cvMyProfile.setOnClickListener(new View.OnClickListener() {
+        cvMyProfile.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final View view = View.inflate(getContext(), R.layout.botom_sheets, null);
-                TextView view_user = view.findViewById(R.id.viewuser);
-                TextView edt_user = view.findViewById(R.id.edtuser);
-                Button btnhuy = view.findViewById(R.id.Huy);
-                final Dialog mBotomsheet = new Dialog(getContext(), R.style.MaterialDialogSheet);
-                mBotomsheet.setContentView(view);
-                mBotomsheet.setCancelable(true);
-                mBotomsheet.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                mBotomsheet.getWindow().setGravity(Gravity.BOTTOM);
-                mBotomsheet.show();
-
-                view_user.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mBotomsheet.dismiss();
-                        final AlertDialog dialog = new AlertDialog.Builder(getActivity()).create();
-                        final View view = View.inflate(getActivity(), R.layout.view_user, null);
-                        dialog.setView(view);
-                        dialog.show();
-
-
-                    }
-                });
-                edt_user.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mBotomsheet.dismiss();
-                        final AlertDialog dialog = new AlertDialog.Builder(getContext()).create();
-                        final View view = View.inflate(getContext(), R.layout.edit_user, null);
+                        final AlertDialog dialog = new AlertDialog.Builder( getContext() ).create();
+                        final View view = View.inflate( getContext(), R.layout.edit_user, null );
                         TextInputEditText edtEmail, edtBirthday;
                         Button btnAddInfo, btnCancleInfo;
-                        edtName = view.findViewById(R.id.edtName);
-                        edtEmail = view.findViewById(R.id.edtEmail);
-                        edtBirthday = view.findViewById(R.id.edtBirthday);
-                        edtimage = view.findViewById(R.id.edtimage);
-                        btnAddInfo = view.findViewById(R.id.btnAddInfo);
-                        btnCancleInfo = view.findViewById(R.id.btnCancleInfo);
+                        edtName = view.findViewById( R.id.edtName );
+                        edtEmail = view.findViewById( R.id.edtEmail );
+                        edtBirthday = view.findViewById( R.id.edtBirthday );
+                        edtimage = view.findViewById( R.id.img );
+                        imgcapture=view.findViewById( R.id.imgcapture );
+                        btnAddInfo = view.findViewById( R.id.btnAddInfo );
+                        btnCancleInfo = view.findViewById( R.id.btnCancleInfo );
                         ModelUser nameimg1 = dbHelper.getUser();
-                        edtBirthday.setText(nameimg1.getUserBirthday());
-                        edtName.setText(nameimg1.getUserName());
-                        edtEmail.setText(nameimg1.getUserMail());
-
-                        Uri imguri = Uri.parse(nameimg.getUserImage());
-
-                        try {
-                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imguri);
-                            edtimage.setImageBitmap(bitmap);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-
-                        edtimage.setOnClickListener(new View.OnClickListener() {
+                        Picasso.get().load(nameimg1.getUserImage()  ).into( edtimage );
+                        edtName.setText( nameimg1.getUserName() );
+                        edtEmail.setText( nameimg1.getUserMail() );
+                        edtBirthday.setText( nameimg1.getUserBirthday() );
+                        imgcapture.setOnClickListener( new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                startActivityForResult(intent, Image_Capture_Code);
+                                Intent intent = new Intent( MediaStore.ACTION_IMAGE_CAPTURE );
+                                startActivityForResult( intent, Image_Capture_Code );
 
 
                             }
-                        });
-                        btnAddInfo.setOnClickListener(new View.OnClickListener() {
+                        } );
+                        btnAddInfo.setOnClickListener( new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                final HttpAdapter adapter = new HttpAdapter();
-                                adapter.setBaseUrl(url);
-                                action = adapter.create(SystemService.class);
-                                ModelStatusUser update = action.updateUser(dbHelper.getUser().getUserPhone(), edtName.getText().toString(), edtEmail.getText().toString(), edtBirthday.getText().toString(), String.valueOf(uri));
-                                dbHelper.update(String.valueOf(dbHelper.getUser().getUserPhone()), edtName.getText().toString(), edtEmail.getText().toString(), edtBirthday.getText().toString(), String.valueOf(uri));
-                                Toast.makeText(getActivity(), update.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+
+                                FirebaseStorage storage = FirebaseStorage.getInstance();
+                                StorageReference storageReference = storage.getReference();
+                                StorageReference ref = storageReference.child( "images/" + UUID.randomUUID().toString() );
+
+                                // adding listeners on upload
+                                // or failure of image
+                                ref.putFile( uri )
+                                        .addOnSuccessListener(
+                                                new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                                    @Override
+                                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                                        Task<Uri> downloadUri = taskSnapshot.getStorage().getDownloadUrl();
+//Lay liink an
+                                                        while ((!downloadUri.isSuccessful())) ;
+                                                        linkImage = downloadUri.getResult().toString();
+                                                        final HttpAdapter adapter = new HttpAdapter();
+                                                        adapter.setBaseUrl( url );
+                                                        action = adapter.create( SystemService.class );
+                                                        ModelStatusUser update = action.updateUser( dbHelper.getUser().getUserPhone(), edtName.getText().toString(), edtEmail.getText().toString(), edtBirthday.getText().toString(), linkImage );
+                                                        dbHelper.update( String.valueOf( dbHelper.getUser().getUserPhone() ), edtName.getText().toString(), edtEmail.getText().toString(), edtBirthday.getText().toString(), linkImage );
+                                                        Toast.makeText( getActivity(), update.getMessage(), Toast.LENGTH_SHORT ).show();
+                                                        dialog.dismiss();
+                                                    }
+                                                } )
+
+                                        .addOnFailureListener( new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+
+                                                // Error, Image not uploaded
+                                            }
+                                        } )
+                                        .addOnProgressListener(
+                                                new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                                    // Progress Listener for loading
+                                                    // percentage on the dialog box
+                                                    @Override
+                                                    public void onProgress(
+                                                            UploadTask.TaskSnapshot taskSnapshot) {
+                                                        double progress
+                                                                = (100.0
+                                                                * taskSnapshot.getBytesTransferred()
+                                                                / taskSnapshot.getTotalByteCount());
+                                                    }
+                                                } );
+                            }
+                        } );
+                        btnCancleInfo.setOnClickListener( new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
                                 dialog.dismiss();
-
-
                             }
-                        });
-                        btnCancleInfo.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                dialog.dismiss();
-                            }
-                        });
+                        } );
 
-                        dialog.setView(view);
+                        dialog.setView( view );
                         dialog.show();
                     }
-                });
-                btnhuy.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mBotomsheet.dismiss();
-                    }
-                });
-            }
-        });
+                } );
 
-// chuyển inforOrder Fragment
-        cvFavoriteFood.setOnClickListener(new View.OnClickListener() {
+//// chuyển inforOrder Fragment
+//        cvFavoriteFood.setOnClickListener( new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentManager fm = getFragmentManager();
+//                FragmentTransaction ft = fm.beginTransaction();
+//                InforOderFragment inforOderFragment = new InforOderFragment();
+//                ft.replace( R.id.frame_container, inforOderFragment );
+//                ft.addToBackStack( null );
+//                MainActivity.navigationView.setVisibility( View.GONE );
+//                ft.commit();
+//            }
+//        } );
+
+        question.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                InforOderFragment inforOderFragment = new InforOderFragment();
-                ft.replace(R.id.frame_container, inforOderFragment);
-                ft.addToBackStack(null);
-                MainActivity.navigationView.setVisibility(View.GONE);
-                ft.commit();
+
             }
-        });
+        } );
         return view;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult( requestCode, resultCode, data );
         if (requestCode == Image_Capture_Code) {
             if (resultCode == RESULT_OK) {
-                Bitmap bp = (Bitmap) data.getExtras().get("data");
-                edtimage.setImageBitmap(bp);
-                uri = Uri.parse(MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), bp, null, null));
-                Log.d("abc", "kkkk" + uri.toString());
+                Bitmap bp = (Bitmap) data.getExtras().get( "data" );
+                edtimage.setImageBitmap( bp );
+                uri = Uri.parse( MediaStore.Images.Media.insertImage( getActivity().getContentResolver(), bp, null, null ) );
             } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+                Toast.makeText( getActivity(), "Cancelled", Toast.LENGTH_LONG ).show();
             }
         }
     }
